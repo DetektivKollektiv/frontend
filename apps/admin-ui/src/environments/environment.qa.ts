f83ab9a7d0c4c58751ca6eaf3fe6f874d55c7b33
SandroGlueck <sandro.glueck@gmail.com>
2021-02-23 23:51:13 +0100

    Adapted client id

--- apps/admin-ui/src/environments/environment.qa.ts
+++ apps/admin-ui/src/environments/environment.qa.ts
@@ -6,6 +6,6 @@ export const environment = {
     identityPoolId: 'eu-central-1:434fdf6d-d2e6-41df-a7cb-0301a8b91bf7',
     region: 'eu-central-1',
     userPoolId: 'eu-central-1_Uk1f9Sydt',
-    clientId: '4f4mq2u2u661bdmhn8df611i92',
+    clientId: '663lfejqo5gh51ien9l2hkligb',
   },
 };
