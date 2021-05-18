55d35b98a1fee63be58f0828e5ffeed8449a859b
SandroGlueck <sandro.glueck@gmail.com>
2021-05-18 21:20:14 +0200

    Added Item Type to items overview: * added column to table
    Misc:
    * Exchanged favicon
    * added pie chart to dashboard
    * refactored dashboard code

--- apps/admin-ui/src/app/dashboard/dashboard.component.ts
+++ apps/admin-ui/src/app/dashboard/dashboard.component.ts
@@ -1,7 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Select } from '@ngxs/store';
 import { Observable } from 'rxjs';
-import { Item } from '../model/item';
+import { GraphData } from '../model/graphData';
 import { ItemsState } from '../store/items/items.state';
 
 @Component({
@@ -9,8 +9,22 @@ import { ItemsState } from '../store/items/items.state';
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss'],
 })
-export class DashboardComponent {
-  @Select(ItemsState.openItems) openItems$: Observable<Item[]>;
-  @Select(ItemsState.closedItems) closedItems$: Observable<Item[]>;
-  @Select(ItemsState.unconfirmedItems) unconfirmedItems$: Observable<Item[]>;
+export class DashboardComponent implements OnInit {
+  @Select(ItemsState.groupedItems) groupedItems$: Observable<string[]>;
+
+  public data: GraphData[];
+
+  constructor() {
+    this.data = [];
+  }
+
+  ngOnInit(): void {
+    this.groupedItems$.subscribe((items) => {
+      Object.keys(items).forEach((i) => {
+        if (this.data.every((d) => d.name !== i)) {
+          this.data.push({ name: i, value: items[i].length });
+        }
+      });
+    });
+  }
 }
